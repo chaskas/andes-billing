63162a168191435cb51c53096282ffbb246f5f7c
Rodrigo Campos <ricamphe@gmail.com>
2024-12-08 17:40:19 +0100

    Sets Invoices number before save

--- app/models/billing/invoice.rb
+++ app/models/billing/invoice.rb
@@ -4,5 +4,22 @@ module Billing
     belongs_to :recipient, class_name: "Billing::Recipient", foreign_key: "billing_recipient_id"
 
     validates :issue_date, :billing_issuer_id, :billing_recipient_id, presence: true
+
+    before_save :set_number
+
+    private
+
+      def get_last_number_for_year(year)
+        last_invoice = Invoice.where("extract(year from issue_date) = ?", year).order(:number).last
+        if last_invoice
+          last_invoice.number
+        else
+          0
+        end
+      end
+
+    def set_number
+      self.number = get_last_number_for_year(self.issue_date.year) + 1
+    end
   end
 end
