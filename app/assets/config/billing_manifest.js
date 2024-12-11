1c85d4b2a171c56e364022df99853a0f765f01c4
Rodrigo Campos <ricamphe@gmail.com>
2024-12-11 14:30:10 +0100

    Adds turbo-rails !

--- app/assets/config/billing_manifest.js
+++ app/assets/config/billing_manifest.js
@@ -1,2 +1,3 @@
 //= link_directory ../builds .css
 //= link_directory ../../javascript/billing .js
+//= link_directory ../../javascript/billing/controllers .js
--- app/javascript/billing/application.js
+++ app/javascript/billing/application.js
@@ -1,2 +1,3 @@
 import * as bootstrap from "bootstrap"
 import "@hotwired/turbo-rails"
+import "controllers"
