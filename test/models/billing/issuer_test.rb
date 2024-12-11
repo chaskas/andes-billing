dd7299da68b749605a0177c6b2dbeebeb1f32046
Rodrigo Campos <ricamphe@gmail.com>
2024-12-11 22:01:46 +0100

    WIP: fixing tests

--- test/models/billing/issuer_test.rb
+++ test/models/billing/issuer_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 module Billing
   class IssuerTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "the truth" do
+      assert true
+    end
   end
 end
