d106e50da39fdeb38b63fe92fd5368d05cab4168
Rodrigo Campos <ricamphe@gmail.com>
2024-12-12 16:59:51 +0100

    testing pre-commit for rails tests

--- test/controllers/billing/invoices_controller_test.rb
+++ test/controllers/billing/invoices_controller_test.rb
@@ -1,6 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 module Billing
+  # Test suite for the InvoicesController
   class InvoicesControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
@@ -8,19 +11,29 @@ module Billing
       @invoice = billing_invoices(:one)
     end
 
-    test "should get index" do
+    test 'should get index' do
       get invoices_url
-      assert_response :new
+      assert_response :success
     end
 
-    test "should get new" do
+    test 'should get new' do
       get new_invoice_url
       assert_response :success
     end
 
-    test "should create invoice" do
-      assert_difference("Invoice.count") do
-        post invoices_url, params: { invoice: { gross_total: @invoice.gross_total, issue_date: @invoice.issue_date, billing_issuer_id: @invoice.issuer.id, net_total: @invoice.net_total, number: @invoice.number, billing_recipient_id: @invoice.recipient.id, status: @invoice.status, tax_amount: @invoice.tax_amount, tax_rate: @invoice.tax_rate } }
+    test 'should create invoice' do
+      assert_difference('Invoice.count') do
+        post invoices_url, params: { invoice: {
+          gross_total: @invoice.gross_total,
+          issue_date: @invoice.issue_date,
+          billing_issuer_id: @invoice.issuer.id,
+          net_total: @invoice.net_total,
+          number: @invoice.number,
+          billing_recipient_id: @invoice.recipient.id,
+          status: @invoice.status,
+          tax_amount: @invoice.tax_amount,
+          tax_rate: @invoice.tax_rate
+        } }
       end
 
       last_invoice_created = Billing::Invoice.order(created_at: :asc).last
@@ -28,18 +41,28 @@ module Billing
       assert_redirected_to edit_invoice_url(last_invoice_created.id)
     end
 
-    test "should get edit" do
+    test 'should get edit' do
       get edit_invoice_url(@invoice)
       assert_response :success
     end
 
-    test "should update invoice" do
-      patch invoice_url(@invoice), params: { invoice: { gross_total: @invoice.gross_total, issue_date: @invoice.issue_date, issuer_id: @invoice.issuer.id, net_total: @invoice.net_total, number: @invoice.number, recipient_id: @invoice.recipient.id, status: @invoice.status, tax_amount: @invoice.tax_amount, tax_rate: @invoice.tax_rate } }
+    test 'should update invoice' do
+      patch invoice_url(@invoice), params: { invoice: {
+        gross_total: @invoice.gross_total,
+        issue_date: @invoice.issue_date,
+        issuer_id: @invoice.issuer.id,
+        net_total: @invoice.net_total,
+        number: @invoice.number,
+        recipient_id: @invoice.recipient.id,
+        status: @invoice.status,
+        tax_amount: @invoice.tax_amount,
+        tax_rate: @invoice.tax_rate
+      } }
       assert_redirected_to invoices_url
     end
 
-    test "should destroy invoice" do
-      assert_difference("Invoice.count", -1) do
+    test 'should destroy invoice' do
+      assert_difference('Invoice.count', -1) do
         delete invoice_url(@invoice)
       end
 
