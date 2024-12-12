3d754e926a2f2f2f2ebc120d504035ffc3061804
Rodrigo Campos <ricamphe@gmail.com>
2024-12-12 15:44:39 +0100

    fix dummy database issues

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,14 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_07_173043) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_11_165004) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "billing_invoice_items", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "billing_invoice_id", null: false
+    t.datetime "date"
+    t.integer "duration"
+    t.integer "kind"
+    t.decimal "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["billing_invoice_id"], name: "index_billing_invoice_items_on_billing_invoice_id"
+  end
+
+  create_table "billing_invoices", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "issue_date", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.integer "status"
+    t.integer "number", null: false
+    t.bigint "billing_issuer_id", null: false
+    t.bigint "billing_recipient_id", null: false
+    t.decimal "net_total"
+    t.integer "tax_rate"
+    t.decimal "tax_amount"
+    t.decimal "gross_total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["billing_issuer_id"], name: "index_billing_invoices_on_billing_issuer_id"
+    t.index ["billing_recipient_id"], name: "index_billing_invoices_on_billing_recipient_id"
+  end
+
   create_table "billing_issuers", force: :cascade do |t|
     t.string "name"
-    t.string "taxNumber"
+    t.string "tax_number"
     t.string "email"
     t.string "address"
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "billing_recipients", force: :cascade do |t|
+    t.string "name"
+    t.string "tax_number"
+    t.string "email"
+    t.string "phone"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "billing_invoice_items", "billing_invoices"
+  add_foreign_key "billing_invoices", "billing_issuers"
+  add_foreign_key "billing_invoices", "billing_recipients"
 end
