b85b9aacd45b4a7b55cd3a9eb27387a9d80d579a
Rodrigo Campos <ricamphe@gmail.com>
2025-03-23 19:22:44 +0100

    feat(model): adds validates on all issuer fields

--- app/models/billing/issuer.rb
+++ app/models/billing/issuer.rb
@@ -2,6 +2,8 @@ module Billing
   class Issuer < ApplicationRecord
     has_many :invoices, foreign_key: "billing_issuer_id", dependent: :destroy
 
+    validates :tax_number, :name, :address, :phone, :email, presence: true
+
     has_one :bank_account, as: :holder, dependent: :destroy
   end
 end
