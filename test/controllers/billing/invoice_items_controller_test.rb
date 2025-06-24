178e7f1808fb643641e9c5f9e8ff37a8a8beae71
Rodrigo Campos <ricamphe@gmail.com>
2025-06-24 19:17:23 +0200

    Fix tests

--- test/controllers/billing/invoice_items_controller_test.rb
+++ test/controllers/billing/invoice_items_controller_test.rb
@@ -8,6 +8,7 @@ module Billing
 
     setup do
       @invoice = billing_invoices(:one)
+      @invoice_item = billing_invoice_items(:one)
       @routes = Engine.routes
     end
 
@@ -46,6 +47,70 @@ module Billing
       assert_response :unprocessable_entity
     end
 
+    test "should create invoice_item with turbo stream format" do
+      assert_difference("InvoiceItem.count") do
+        post invoice_invoice_items_url(@invoice), params: {
+          invoice_item: {
+            date: Date.today,
+            duration: 2,
+            kind: "normal",
+            unit_price: 15.99
+          },
+          turbo_stream: "true",
+          format: :turbo_stream
+        }
+      end
+
+      assert_response :success
+      assert_match /turbo-stream/, @response.body
+      assert_match /invoice_items/, @response.body
+      assert_match /invoice_totals/, @response.body
+      assert_match /new_invoice_item/, @response.body
+      assert_match /flash/, @response.body
+    end
+
+    test "should create invoice_item with turbo stream format without turbo_stream parameter" do
+      assert_difference("InvoiceItem.count") do
+        post invoice_invoice_items_url(@invoice), params: {
+          invoice_item: {
+            date: Date.today,
+            duration: 2,
+            kind: "normal",
+            unit_price: 15.99
+          },
+          format: :turbo_stream
+        }
+      end
+
+      assert_redirected_to edit_invoice_url(@invoice)
+    end
+
+    test "should handle empty params" do
+      assert_no_difference("InvoiceItem.count") do
+        post invoice_invoice_items_url(@invoice), params: {}
+      end
+
+      assert_response :unprocessable_entity
+    end
+
+    test "should not create invoice_item with turbo stream format and invalid attributes" do
+      assert_no_difference("InvoiceItem.count") do
+        post invoice_invoice_items_url(@invoice), params: {
+          invoice_item: {
+            date: nil,
+            duration: nil,
+            kind: nil,
+            unit_price: nil
+          },
+          format: :turbo_stream
+        }
+      end
+
+      assert_response :success
+      assert_match /turbo-stream/, @response.body
+      assert_match /new_invoice_item/, @response.body
+    end
+
     test "should remove date validation error when date is provided" do
       # First submit with all fields empty
       post invoice_invoice_items_url(@invoice), params: {
@@ -71,6 +136,30 @@ module Billing
 
       # The response should not contain the date validation error
       assert_select "li", { count: 0, text: "Date can't be blank" }
+    end
+
+    test "should destroy invoice_item" do
+      invoice = Billing::Invoice.find(@invoice_item.billing_invoice_id)
+      assert_difference("InvoiceItem.count", -1) do
+        delete invoice_invoice_item_url(invoice, @invoice_item)
+      end
+
+      assert_redirected_to edit_invoice_url(invoice)
+      assert_equal "Item was successfully destroyed.", flash[:notice]
+    end
+
+    test "should destroy invoice_item with turbo stream format" do
+      invoice = Billing::Invoice.find(@invoice_item.billing_invoice_id)
+      assert_difference("InvoiceItem.count", -1) do
+        delete invoice_invoice_item_url(invoice, @invoice_item), params: { format: :turbo_stream }
+      end
+
+      assert_response :success
+      assert_match /turbo-stream/, @response.body
+      assert_match /invoice_items/, @response.body
+      assert_match /invoice_totals/, @response.body
+      assert_match /flash/, @response.body
+      assert_match /Item was successfully removed/, @response.body
     end
   end
 end
